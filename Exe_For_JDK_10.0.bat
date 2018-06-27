c82e5f5bfc52baf48bf3d4596a1278a4e32c8ed5
codewarriors0001 <codewarriors0001@gmail.com>
2018-06-27 15:55:19 +0530

    Created Executable batch files for Java JDK 1.8 & JDK 10.0

---
+++ Exe_For_JDK_10.0.bat
@@ -0,0 +1,6 @@
+for /f "tokens=5" %%a in ('netstat -aon ^| find "8080"') do taskkill /f /pid %%a
+call mvnw clean package
+echo Exit Code = %ERRORLEVEL%
+if not "%ERRORLEVEL%" == "0" exit /b
+call java -jar --add-modules java.xml.bind target/SMS_Codewarriors-0.0.1-SNAPSHOT.jar
+pause >nul
